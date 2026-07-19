44c51b2bf301ddc6e7e4dc39c5e295592ecf0b7e
keitamady <sereboria100@gmail.com>
2026-07-19 14:11:02 +0000

    adding

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -18,6 +18,21 @@ const Project = () => {
            <a className='demo' href="https://my-first-todo-list-teal.vercel.app/">live demo</a>
             <a className='github' href="https://github.com/keitamady/todo">github</a>
         </div>
+      </div> 
+
+
+      <div className="project-items">
+        <div className="images">
+          <img src="imgs/weather.jpg" alt="images" />
+          <div className="names">
+            <h3 className='name'>weather app</h3>
+            <span className='create-with'>react | css</span>
+          </div>
+        </div>
+        <div className="links">
+           <a className='demo' href="https://weather-xi-henna.vercel.app/">live demo</a>
+            <a className='github' href="https://github.com/keitamady/weather">github</a>
+        </div>
       </div>
       </div>
     </div>
